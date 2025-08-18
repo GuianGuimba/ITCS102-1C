9368f05471f521c8485c35924bb58b8d9f497f1f
Guian Guimba <esteebean31@gmail.com>
2025-08-18 10:12:00 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Guian Esteebean A. Guimba 
+BSIT-1C
+025A-13846
+
+Hello! im Guian Esteebean A. Guimba 18years old, from Brgy. Cotta Lucena City!
--- README.py
+++
@@ -1,3 +0,0 @@
-print("Guian Esteebean A. Guimba \tBSIT-1C \t025A-13846")
-
-print("Hello! im Guian Esteebean A. Guimba 18years old, from Brgy. Cotta Lucena City!")
