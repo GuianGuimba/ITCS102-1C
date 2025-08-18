d9e51b32d2966134546fd4e08902d61cb53f74c0
Guian Guimba <esteebean31@gmail.com>
2025-08-18 10:11:24 +0800

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,5 +0,0 @@
-Guian Esteebean A. Guimba
-BSIT-1C
-025A-13846
-
-Hello! im Guian Esteebean A. Guimba 18years old, from Brgy. Cotta Lucena City!
---
+++ README.py
@@ -0,0 +1,3 @@
+print("Guian Esteebean A. Guimba \tBSIT-1C \t025A-13846")
+
+print("Hello! im Guian Esteebean A. Guimba 18years old, from Brgy. Cotta Lucena City!")
