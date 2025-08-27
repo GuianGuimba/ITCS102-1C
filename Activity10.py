4c3583d3c422c7966af624b64bff95e5db83e46a
Guian Guimba <esteebean31@gmail.com>
2025-08-27 13:57:02 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,12 @@
+#from getpass import getpass
+
+username = 'Guian'
+pword = '12345'
+
+u = input("Username ==>")
+p = input("Password ==>")
+
+if (u == username)and(p == pword) :
+	print("Access Granted")
+else :
+	print("Access Denied")
