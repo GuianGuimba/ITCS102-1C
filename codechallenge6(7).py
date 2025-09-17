2ed54e03a1990c1f00d37a441b66189510bb89f6
Guian Guimba <esteebean31@gmail.com>
2025-09-17 11:28:04 +0800

    Create codechallenge6(7).py

---
+++ codechallenge6(7).py
@@ -0,0 +1,7 @@
+odd = 0
+for n in range(1,5,1):
+    numbers = eval(input("Input numbers here ==> "))
+    if numbers % 2 != 0:
+        odd += numbers
+    
+print("The sum of odd numbers you inputed is",odd)
