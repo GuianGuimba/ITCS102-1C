d9a3b8c1adc1ed055c05ba749ef53bf3cd22adef
Guian Guimba <esteebean31@gmail.com>
2025-10-01 12:13:27 +0800

    Update codechallenge7(8).py

--- codechallenge7(8).py
+++ codechallenge7(8).py
@@ -1,5 +1,5 @@
 number = eval(input("MULTIPLICATION TABLE MAKER\nEnter a Number ==> "))
 print("Multiplication for",number,"is..")
 for n in range(1,11,1):
-    x *= number * n
+    x = number * n
     print(number,"x",n,"=",x)
