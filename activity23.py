92bb473ba15e5f6d7f16f3dccbb8ee0d3c7a295e
Guian Guimba <esteebean31@gmail.com>
2025-11-12 14:06:40 +0800

    Add greetings and function calls in activity23.py

---
+++ activity23.py
@@ -0,0 +1,12 @@
+from activity23_1 import another, NLA, Return, Factorial
+
+def Greetings():
+    print("Hi! This is a Function\nWhat it does is Greet People\nHi VISITOR")
+    
+Greetings()
+another('Guian')
+NLA('Guian','Lucena','67')
+
+print(f"i want to get the summation of 1 to {Return(8)}")
+print(f"i want to get the summation of 1 to {Return(8)+10}")
+print(f"The factorial of 3 is {Factorial(3)}")
