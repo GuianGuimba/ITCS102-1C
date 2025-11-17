81d220b9df0129552f7fde4109767e90878fa638
Guian Guimba <esteebean31@gmail.com>
2025-11-17 14:32:09 +0800

    Add list and dictionary for ML heroes

---
+++ activity25.py
@@ -0,0 +1,11 @@
+#LIST
+
+Hero_sa_ML_Roamer = ['Minotour','Tigreal','Khufra','Grock','Kaja',"Belerick"]
+print(Hero_sa_ML_Roamer[3])
+
+#DICTIONARY
+
+Type_ng_Hero_sa_ML_Roamer = {'Minotour':'Setter and Heal','Tigreal':'Setter Tank','Khufra':'Setter Tank',\
+                             'Grock':'Setter Tank','Kaja':'Suppresed Support','Belerick':'Setter Tank'}
+
+print(Type_ng_Hero_sa_ML_Roamer['Grock'])
